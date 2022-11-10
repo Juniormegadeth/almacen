37a3424d20d53017e6468de911a4e54f81c1820d
Juniormegadeth <117453437+Juniormegadeth@users.noreply.github.com>
2022-11-10 18:46:06 -0500

    Frontend
    Actualizacion

---
+++ taller/TallerHoteles/hoteles/apps/hoteles/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from apps.hoteles.models import Hoteles
+# Register your models here.
+
+class MembershipInline(admin.TabularInline):
+    model = Hoteles
+    extra = 1
+
+class HotelesAdmin(admin.ModelAdmin):
+    inline = (MembershipInline, )
+    list_display = ()
+    search_fields = () #Campo relacionado
+    list_filter = ()
+
+
+admin.site.register(Hoteles)
+admin.site.register(Hoteles, HotelesAdmin)
